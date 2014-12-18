9430027b0e2cf3fad57dae6614d83165d3c36c15
philippeback <phil@highoctane.be>
2014-12-18 16:21:20 +0100

    cleanup

--- get-data.R
+++ get-data.R
@@ -1,16 +1,8 @@
-# Load the data
+# Get the data
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-
 dest <- "./data/power.zip"
-
 dir.create("./data", showWarnings=FALSE)
-
 if (file.exists(dest)) file.remove(dest)
-
 download.file(url, destfile=dest, method="curl")
-
 #unzip overwrites by default
-
 unzip("./data/power.zip")
-
-dateDownloaded <- date()
--- load-data.R
+++ load-data.R
@@ -4,7 +4,6 @@
 library(lubridate)
 
 data <- read.table("./household_power_consumption.txt", sep=";", header=TRUE)
-# data$Time<-strptime(paste(data$Date, data$Time,"%H:%M:%S")
 data$DateTime <- dmy_hms(paste(data$Date, data$Time))
 data$Date <- as.Date(data$Date,"%d/%m/%Y")
 dataset <- data[which((data$Date == as.Date("2007-02-01")) | (data$Date == as.Date("2007-02-02"))), ]
