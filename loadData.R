64734434ff01e9cce13f63457eebd28f40ee1b74
philippeback <phil@highoctane.be>
2014-12-18 15:48:59 +0100

    removed remnants

--- loadData.R
+++
@@ -1,4 +0,0 @@
-data <- read.table("./household_power_consumption.txt", sep=";", header=TRUE)
-data$Date <- as.Date(data$Date,"%d/%m/%Y")
-data$Time<-strptime(data$Time,"%H:%M:%S")
-dataset <- data[which((data$Date == as.Date("2007-02-01")) | (data$Date == as.Date("2007-02-02"))), ]
